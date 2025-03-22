7cf656da7a8f5ba26d0663bbb88a8587e197aefb
Admin <Admin@Nitein-Kurane>
2025-03-22 09:21:48 +0530

    Checking number is Palindrome or not

---
+++ Assignments/src/assignment_16_march/Check_number_is_palindrome.java
@@ -0,0 +1,47 @@
+package assignment_16_march;
+
+import java.lang.reflect.Array;
+
+public class Check_number_is_palindrome {
+	
+	
+	public void checkNumberIsPalindrome(int a)
+	
+	{
+		
+		System.out.println("Given Number is "+ a);
+		
+		System.out.println("Checking number is Palindrome or not");
+		
+		int givenNUmber =a;
+		int reversedNumber = 0;
+		
+        while (a > 0) {
+            int digit = a % 10; // Get last digit
+            reversedNumber = reversedNumber * 10 + digit; // Append to reversedNumber
+            a = a / 10; // Remove last digit
+        }
+        	
+		
+		
+		if(givenNUmber ==reversedNumber)
+		{
+			System.out.println("Given Number "+ givenNUmber + " is a Palindrome number");
+
+		}else
+		{
+			System.out.println("Given Number "+ givenNUmber + " is not a Palindrome number");
+
+		}
+		
+	}	
+
+
+	public static void main(String[] args) {
+		
+		Check_number_is_palindrome chp = new Check_number_is_palindrome();
+		chp.checkNumberIsPalindrome(222);
+
+		}
+
+}
